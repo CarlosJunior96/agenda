9e0ed2b444a1037a340dd684023b1d39828a3c41
carlos.dasilva <carlos.dasilva@goias.gov.br>
2021-03-09 15:38:38 -0300

    comit 2

--- db/migrate/20210308194322_create_kinds.rb
+++ db/migrate/20210308194322_create_kinds.rb
@@ -3,7 +3,7 @@ class CreateKinds < ActiveRecord::Migration[6.1]
     create_table :kinds do |t|
       t.string :description
 
-      t.timestamps
+      t.timestamps null: false #created_at, update_at
     end
   end
 end
