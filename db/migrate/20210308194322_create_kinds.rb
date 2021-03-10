4f1d971f2e0c0a24b1afbe493d323fc7cbf9d499
Carlos Junior Silva <cjrcordeiro@gmail.com>
2021-03-10 12:01:53 -0300

    teste migrate

--- db/migrate/20210308194322_create_kinds.rb
+++ db/migrate/20210308194322_create_kinds.rb
@@ -1,7 +1,7 @@
 class CreateKinds < ActiveRecord::Migration[6.1]
   def change
     create_table :kinds do |t|
-      t.string :description
+      t.string :descrition
 
       t.timestamps null: false #created_at, update_at
     end
